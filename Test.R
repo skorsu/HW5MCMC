025c271b5c2d526f97fffc5f81dbb2bc748ecb72
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-03-24 23:26:31 -0600

    code for Gibbs via Rcpp

--- Test.R
+++ Test.R
@@ -4,112 +4,62 @@ library(RcppArmadillo)
 library(foreach)
 library(doParallel)
 library(Deriv)
+library(nimble)
+
+model <- nimbleCode({
+  # likelihood
+  survived ~ dbinom(theta, released)
+  # prior
+  theta ~ dunif(0, 1)
+  # derived quantity
+  lifespan <- -1/log(theta)
+})
+
+
+
+
+
 
 path <- "/Users/kevin-imac/Desktop/Github - Repo/"
 if(! file.exists(path)){
   path <- "/Users/kevinkvp/Desktop/Github Repo/"
 }
 
-sourceCpp(paste0(path, "HW3EM/src/main.cpp"))
+dat <- read.table(paste0(path, "HW5MCMC/coal.dat"), header = TRUE)
 
-### User-defined functions
-meanSD <- function(x, dplace = 5){
-  mm <- round(mean(x), digits = dplace)
-  ss <- round(sd(x), digits = dplace)
-  paste0(mm, " (SD = ", ss, ")")
+sourceCpp(paste0(path, "HW5MCMC/src/main.cpp"))
+set.seed(10)
+result <- gibbsGamma(iter = 50000, dat = dat$disasters)
+
+result <- gibbsHalfN(iter = 50000, s2_1 = 1, s2_2 = 1, dat = dat$disasters)
+
+for(i in 2:111){
+  c(var(dat$disasters[1:i]), var(dat$disasters[-(1:i)])) %>% print()
 }
 
-### Simulated the data
-set.seed(31082, kind = "L'Ecuyer-CMRG")
-registerDoParallel(5)
-simDat <- foreach(t = 1:100) %dopar% {
-  
-  ### Simulate the data
-  clus_ind <- rbinom(100, 1, 0.25)
-  y <- rexp(100, rate = ifelse(clus_ind == 1, 1, 2))
-  
-  y
-  
-}
-stopImplicitCluster()
-
-### Run the model
-set.seed(31082, kind = "L'Ecuyer-CMRG")
-registerDoParallel(5)
-resultEM <- foreach(t = 1:100, .combine = "rbind") %dopar% {
-  
-  em_result <- EM_rcpp(y = simDat[[t]], p0 = 0.25, lambda0 = 1, mu0 = 2, eps = 1e-10)
-  c(em_result$p, em_result$lambda, em_result$mu)
-  
-}
-stopImplicitCluster()
-
-resultEM
-
-### Louis SE
-set.seed(31082, kind = "L'Ecuyer-CMRG")
-registerDoParallel(5)
-VARLouis <- foreach(t = 1:100, .combine = "rbind") %dopar% {
-  
-  varMat <- iY(y = simDat[[t]], p = resultEM[t, 1], lb = resultEM[t, 2], mu = resultEM[t, 3]) - 
-    iX(y = simDat[[t]], p = resultEM[t, 1], lb = resultEM[t, 2], mu = resultEM[t, 3])
-  1/(diag(varMat))
-  
-}
-stopImplicitCluster()
-
-sqrt(apply(VARLouis, 2, mean))
-
-#### Bootstrap
-set.seed(31082, kind = "L'Ecuyer-CMRG")
-registerDoParallel(5)
-bTest <- foreach(t = 1:100) %:%
-  foreach(m = 1:100) %dopar% {
-  randB <- sample(1:100, size = 100, replace = TRUE)
-  EMBoot <- EM_rcpp(y = simDat[[t]][randB], p0 = 0.25, lambda0 = 1, mu0 = 2, eps = 1e-5)
-  list(randB, c(EMBoot$p, EMBoot$lambda, EMBoot$mu))
-}
-stopImplicitCluster()
-
-sapply(1:100,
-       function(y){apply(t(sapply(1:100, function(x){bTest[[y]][[x]][[2]]})), 2, var)}) %>%
-  t() %>%
-  apply(2, meanSD)
-
-sapply(1:100,
-       function(y){apply(t(sapply(1:100, function(x){bTest[[y]][[x]][[2]]})), 2, var)}) %>%
-  t() %>%
-  .[, 3] %>%
-  boxplot()
-
-var(bTest[, 3])
 
 
-set.seed(31082, kind = "L'Ecuyer-CMRG")
-registerDoParallel(5)
-VARBoots <- foreach(t = 1:100, .combine = "rbind") %dopar% {
-  
-  bootMat <- BootResult(y = simDat[[t]], p0 = 0.25, lambda0 = 1, mu0 = 2, 
-                        eps = 1e-10, EMinit = resultEM[t, ], M = 1000)
-  apply(bootMat, 2, var)
-  
-}
-stopImplicitCluster()
+plot(result[, 1], type = "l")
+plot(result[, 2], type = "l")
+plot(result[, 3], type = "l")
 
-### SEM
-SEM(y = simDat[[3]], p0 = 0.24, lambda0 = 0.99, mu0 = 2.01, eps = 1e-10, EMfinal = resultEM[3, ])
+dat
 
-### Derivative: iX
-lfunction <- function(y, p, lb, mu){
-  sum(log((p * lb * exp(-lb * y)) + ((1 - p) * mu * exp(-mu * y))))
+
+update_theta(theta_old = 10, lambda1 = 0.074, lambda2 = 0.075, alpha = 10, dat = dat$disasters)
+
+
+sampResult <- rep(NA, 100000)
+for(i in 1:100000){
+  sampResult[i] <- update_theta(lambda1 = 0.074, lambda2 = 0.075, alpha = 10, dat = dat$disasters)
 }
 
-Deriv(lfunction, c("p", "lb", "mu")) %>%
-  Deriv(c("p", "lb", "mu"))
+table(sampResult)
 
-Qfunction <- function(y, db, p, lb, mu){
-  sum((db * ((-lb * y) + log(p * lb))) + ((1 - db) * ((-mu * y) + log((1 - p) * mu))))
-}
+testMat <- update_theta(lambda1 = 0.074, lambda2 = 0.075, alpha = 10, dat = dat$disasters)
+testMat
+sum(dat$disasters)
+5^191
+log(rgamma(1, 3, 1)^191)
 
-Deriv(Qfunction, c("p", "lb", "mu")) %>%
-  Deriv(c("p", "lb", "mu"))
+20^140 * 40^51/sum(rep(20^140 * 40^51, 10))
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,158 @@
 #include "RcppArmadillo.h"
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// Target Distribution ---------------------------------------------------------
+// Update Algorithms: ----------------------------------------------------------
 // [[Rcpp::export]]
-double tg_dist_ind(double x, double a, double b){
-  return std::exp(-std::pow(std::abs(x), a)/b);
+int update_theta(int theta_old, double lambda1, double lambda2, arma::vec dat){
+  
+  // Propose a new theta from prior
+  Rcpp::IntegerVector thetapvec = Rcpp::sample(111, 1, false);
+  int theta_propose = thetapvec[0];
+  
+  // Calculate the acceptance probability in the log-scale
+  double lprob = 0.0;
+  lprob += (arma::accu(dat.head_rows(theta_propose)) * std::log(lambda1));
+  lprob += (arma::accu(dat.head_rows(112 - theta_propose)) * std::log(lambda2));
+  lprob -= (-theta_propose * (lambda1 - lambda2));
+  lprob -= (arma::accu(dat.head_rows(theta_old)) * std::log(lambda1));
+  lprob -= (arma::accu(dat.head_rows(112 - theta_old)) * std::log(lambda2));
+  lprob += (-theta_old * (lambda1 - lambda2));
+  
+  // Decide
+  int theta_new = theta_old;
+  double logU = std::log(R::runif(0.0, 1.0));
+  if(logU < lprob){
+    theta_new = theta_propose;
+  }
+  
+  return theta_new;
+  
 }
 
 // [[Rcpp::export]]
-arma::vec tg_dist(arma::vec x, double a, double b){
-  return arma::exp(-arma::pow(arma::abs(x), a)/b);
+double update_l1hn(double l1_old, int theta, double s2_1, arma::vec dat){
+  
+  // Propose a new theta from proposal distribution
+  double l1_propose = R::rgamma(l1_old, 1);
+  
+  // Calculate the acceptance probability in the log-scale
+  double lprob = 0.0;
+  lprob += (arma::accu(dat.head_rows(theta)) * std::log(l1_propose));
+  lprob -= (l1_propose * theta);
+  lprob -= (std::pow(l1_propose, 2.0)/(2 * s2_1));
+  lprob += R::dgamma(l1_old, l1_propose, 1, 1);
+  lprob -= (arma::accu(dat.head_rows(theta)) * std::log(l1_old));
+  lprob += (l1_old * theta);
+  lprob += (std::pow(l1_old, 2.0)/(2 * s2_1));
+  lprob -= R::dgamma(l1_propose, l1_old, 1, 1);
+  
+  // Decide
+  double l1_new = l1_old;
+  double logU = std::log(R::runif(0.0, 1.0));
+  if(logU < lprob){
+    l1_new = l1_propose;
+  }
+  
+  return l1_new;
+  
 }
 
-// Important Sampling ----------------------------------------------------------
 // [[Rcpp::export]]
-arma::mat isRcpp(unsigned int n, double a, double b, double df){
+double update_l2hn(double l2_old, int theta, double s2_2, arma::vec dat){
   
-  arma::mat result(n, 5, arma::fill::zeros);
-  Rcpp::NumericVector xRcpp = Rcpp::rt(n, df); // Envelope distribution: t with df = 3
-  Rcpp::NumericVector fxRcpp = Rcpp::dt(xRcpp, df);
+  // Propose a new theta from proposal distribution
+  double l2_propose = R::rgamma(l2_old, 1);
   
-  arma::vec x = Rcpp::as<arma::vec>(Rcpp::wrap(xRcpp));
-  arma::vec fx = Rcpp::as<arma::vec>(Rcpp::wrap(fxRcpp));
+  // Calculate the acceptance probability in the log-scale
+  double lprob = 0.0;
+  lprob += (arma::accu(dat.head_rows(112 - theta)) * std::log(l2_propose));
+  lprob -= (l2_propose * (112 - theta));
+  lprob -= (std::pow(l2_propose, 2.0)/(2 * s2_2));
+  lprob += R::dgamma(l2_old, l2_propose, 1, 1);
+  lprob -= (arma::accu(dat.head_rows(112 - theta)) * std::log(l2_old));
+  lprob += (l2_old * (112 - theta));
+  lprob += (std::pow(l2_old, 2.0)/(2 * s2_2));
+  lprob -= R::dgamma(l2_propose, l2_old, 1, 1);
   
-  result.col(0) = x;
-  result.col(1) = fx;
-  result.col(2) = tg_dist(x, a, b);
-  result.col(3) = tg_dist(x, a, b)/fx;
+  // Decide
+  double l2_new = l2_old;
+  double logU = std::log(R::runif(0.0, 1.0));
+  if(logU < lprob){
+    l2_new = l2_propose;
+  }
   
-  double sumW = arma::accu(tg_dist(x, a, b)/fx);
-  result.col(4) = result.col(3)/sumW;
+  return l2_new;
+  
+}
+
+// Gibbs Sampler: --------------------------------------------------------------
+// [[Rcpp::export]]
+arma::mat gibbsGamma(int iter, arma::vec dat){
+  
+  arma::mat result(iter, 4, arma::fill::zeros);
+  
+  // Initialize the parameters from prior
+  double ap = R::rgamma(10, 1/10);
+  double l1 = R::rgamma(3, 1/ap);
+  double l2 = R::rgamma(3, 1/ap);
+  Rcpp::IntegerVector thetavec = Rcpp::sample(111, 1, false);
+  int tt = thetavec[0];
+  
+  int tt_new = 0;
+  double l1_new = 0.0;
+  double l2_new = 0.0;
+  double ap_new = 0.0;
+  
+  for(int i = 0; i < iter; ++i){
+    tt_new = update_theta(tt, l1, l2, dat);
+    l1_new = R::rgamma(3 + arma::accu(dat.head_rows(tt_new)), 1/(ap + tt_new));
+    l2_new = R::rgamma(3 + arma::accu(dat.head_rows(112 - tt_new)), 1/(ap + 112 - tt_new));
+    ap_new = R::rgamma(19, 1/(10 + l1_new + l2_new));
+    
+    result.row(i).col(0).fill(tt_new);
+    result.row(i).col(1).fill(l1_new);
+    result.row(i).col(2).fill(l2_new);
+    result.row(i).col(3).fill(ap_new);
+    
+    tt = tt_new;
+    l1 = l1_new;
+    l2 = l2_new;
+    ap = ap_new;
+    
+  }
   
   return result;
   
 }
 
-// Rejection Sampling ----------------------------------------------------------
 // [[Rcpp::export]]
-arma::vec rsRcpp(unsigned int n, double alp, double a, double b, double df){
+arma::mat gibbsHalfN(int iter, double s2_1, double s2_2, arma::vec dat){
   
-  arma::vec result(n, arma::fill::zeros);
+  arma::mat result(iter, 3, arma::fill::zeros);
   
-  unsigned int counter = 0;
-  double y = 0;
-  double u = 0;
-  double e = 0;
+  // Initialize the parameters from prior
+  double l1 = R::rgamma(3, 1);
+  double l2 = R::rgamma(3, 1);
+  Rcpp::IntegerVector thetavec = Rcpp::sample(111, 1, false);
+  int tt = thetavec[0];
   
-  while(counter < n){
-    y = R::rt(df);
-    u = R::runif(0.0, 1.0);
-    e = R::dt(y, df, 0)/alp;
-    if(u <= tg_dist_ind(y, a, b)/e){
-      result.row(counter).fill(y);
-      counter += 1;
-    }
+  int tt_new = 0;
+  double l1_new = 0.0;
+  double l2_new = 0.0;
+  
+  for(int i = 0; i < iter; ++i){
+    tt_new = update_theta(tt, l1, l2, dat);
+    l1_new = update_l1hn(l1, tt_new, s2_1, dat);
+    l2_new = update_l2hn(l2, tt_new, s2_2, dat);
+    
+    result.row(i).col(0).fill(tt_new);
+    result.row(i).col(1).fill(l1_new);
+    result.row(i).col(2).fill(l2_new);
+    
+    tt = tt_new;
+    l1 = l1_new;
+    l2 = l2_new;
+    
   }
   
   return result;
